0f876eacfd20308b1a2610c9e8336ddd5672a295
Jessica  Gomez <jessicagomez@Jessicas-MacBook-Air.local>
2019-05-30 18:03:13 -0700

    seeded data has been inderted into the schema

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+    INSERT INTO burgers (burger_name, devoured)
+    VALUES ("The Crazy Burger", false),
+     ("The Onion Lovers Burger", false), 
+     ("Happily Avo-After", false),
+     ("Cheesy E", false),
+     ("Scicy AF", false);
+
+
+    
